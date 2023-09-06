5c48b6bef12d0a6177ade0b8deab3ef25f935194
Nikita <tim@timrich.org>
2023-09-06 17:56:52 +0300

    коллекция ДЗ

--- main.py
+++ main.py
@@ -1,19 +1,65 @@
-# Генератор рандомных имен с датой и временем
-import names
-import time
-from datetime import datetime
-file_path = "C:/Users/user/PycharmProjects/pythonProject1/test.txt"
-while True:
-    with open(file_path, 'a+') as file_txt:
-        now = datetime.now()
+from tkinter import *
 
-        user_name = names.get_first_name()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-        result_str = user_name + ' -- ' + str(dt_string) + '\n'
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.build()
 
-        file_txt.write(result_str)
+    def build(self):
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
+        self.lbl.place(x=11, y=50)
 
-        file_txt.close()
+        btns = [
+            "C", "DEL", "X^2", "*",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "="
+        ]
 
-    time.sleep(0.5)
+        x = 10
+        y = 140
+        for bt in btns:
+            com = lambda x=bt: self.logicalc(x)
+            Button(text=bt, bg="#FFF",
+                   font=("Times New Roman", 15),
+                   command=com).place(x=x, y=y,
+                                      width=115,
+                                      height=79)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
+
+    def logicalc(self, operation):
+        if operation == "C":
+            self.formula = ""
+        elif operation == "DEL":
+            self.formula = self.formula[0:-1]
+        elif operation == "X^2":
+            self.formula = str((eval(self.formula))**2)
+        elif operation == "=":
+            self.formula = str(eval(self.formula))
+        else:
+            if self.formula == "0":
+                self.formula = ""
+            self.formula += operation
+        self.update()
+
+    def update(self):
+        if self.formula == "":
+            self.formula = "0"
+        self.lbl.configure(text=self.formula)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root["bg"] = "#000"
+    root.geometry("485x550+200+200")
+    root.title("Калькулятор")
+    root.resizable(False, False)
+    app = Main(root)
+    app.pack()
+    root.mainloop()
---
+++ test.py
@@ -0,0 +1,102 @@
+# Урок 1
+# print("Результат:", 5 // 65)
+# print("Результат:", max(5, 6, 7, 8, -11, 33))
+# print("Результат:", round(12 / 7))
+# print('Second \\ Line')
+# input("Введите свой возраст: ")
+
+# number = 5
+# dig = 3.14
+# word = "Результат:"
+# boolean = True
+# str_num = "22"
+
+# print(word + str(dig))
+# print(word + str(number + int(str_num)))
+# del number
+
+# number = 7
+# print("Результат:", number)
+
+
+# num_1 = int(input("Введите первое число: "))
+
+# num_2 = int(input("Введите второе число: "))
+
+# print(word, num_1 + num_2)
+# print(word, num_1 - num_2)
+# print(word, num_1 * num_2)
+# print(word, num_1 / num_2)
+# print(word, num_1 ** num_2)
+
+# print(word, num_1 // num_2)
+
+# user_data = int(input("Введите чиссло: "))
+# if user_data > 5:
+#     print("Число больше чем 5")
+#     if user_data == 12:
+#         print("Число = 12")
+
+
+# user_data = int(input("Введите чиссло: "))
+# isHappy = False
+# if not isHappy and user_data == 6:
+#     print("User is happy")
+# elif user_data == 5:
+#     print('Number is 5')
+# else:
+#     print("User not happy")
+
+
+# data = input()
+
+# number = 5 if data == "Five" else 0
+
+# if data == "Five":
+#     number = 5
+# else:
+#     number = 0
+# print(number)
+
+
+
+# Урок 2
+# item_1 = 144
+# item_0 = 144
+#
+# name = 'Nikita'
+# name_e = ''
+#
+# none_item = None
+# item_list = ['11', 22]
+# item_list_e = []
+#
+# b_item_t = True
+# b_item_f = False
+#
+# compare_item = item_1 < item_0
+#
+# if not compare_item:
+#     print('--1-- True item')
+# elif name_e:
+#     print('--2-- Name item')
+# elif item_list_e:
+#     print('--3-- item_list')
+# else:
+#     print('--- False item')
+# print('________________')
+
+
+
+# Урок 3 (list)
+# l1 = [1, 2, 3, 4, 5]
+# users = ["Anna", "Alex", "Nikita"]
+# l2 = []
+# l3 = [9, 3.14, 'text', True, False, [1,2,3,4], {'name': 'Anna'}]
+# item_1 = 'Yolochka'
+# l4 = list(item_1)
+# print(l4)
+
+
+
+# Урок 4 (питон и базы данных)
